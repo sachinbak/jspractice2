5d3356cfdae81e1707044f32055a05d274f70832
Sachin Kumar Mishra <mishrasachin811@gmail.com>
2020-05-25 09:09:33 +0530

    Add files via upload

---
+++ intract.js
@@ -0,0 +1,8 @@
+//allertt,prompt,console.log
+
+var firstName = prompt("Hello There!! Can you please tell me your First Name");
+var lastName = prompt("hi!! " + firstName + " Please tell me your Second name");
+var age = prompt(firstName + " What is your age?");
+alert("Nice to meet you "  + firstName + " "+ lastName);
+alert("You are still too young. " + age + " is really too small");
+
