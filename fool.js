84fb0a3393c99eb717067558a8433e77c12771d3
Sachin Kumar Mishra <mishrasachin811@gmail.com>
2020-05-26 12:15:27 +0530

    Add files via upload

---
+++ fool.js
@@ -0,0 +1,8 @@
+// annoy the user by proving them fool
+// answer accepted should only be yes
+var answer = prompt("are you a big fool? Type yes if you are... ");
+while(answer != "yes"){
+    var answer = prompt("Admit it that you are fool by saying yes!! :)");
+}
+
+alert(" Hence proved you are a fool. Thanks for keeping your temper in control");
