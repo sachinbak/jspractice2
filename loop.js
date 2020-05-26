fc5f3aefad6cf4117492aff88b830f6448a4f6ec
Sachin Kumar Mishra <mishrasachin811@gmail.com>
2020-05-26 10:37:57 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,22 @@
+var count =1;
+while (count < 11) {
+    console.log(" count is : " + count);
+    count++;
+}
+ console.log("Multiple of 2");
+ var two =2;
+ while (two < 21){
+     console.log("Multiple of two " + two);
+     count +=2;
+ }
+ console.log("Multiple of 2");
+ var t =2, i=1;
+ while (t < 21){
+     while (i<10){
+     console.log("two multiply by  "+ i +"  is equal to " + t);
+     
+     i++;
+     t +=2;
+     }
+     
+ }
