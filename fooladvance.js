78b6a1f0e5c8f43ceb388a18f9a3f6a41889f434
Sachin Kumar Mishra <mishrasachin811@gmail.com>
2020-05-26 12:28:35 +0530

    Add files via upload

---
+++ fooladvance.js
@@ -0,0 +1,8 @@
+// annoy the user by proving them fool
+// answer accepted should be having yes in there sentance yes should be present
+var answer = prompt("are you a big fool? Type yes if you are... ");
+while(answer.indexOf("yes") == -1){
+    var answer = prompt("Admit it that you are fool by saying yes!! :)");
+}
+
+alert(" Hence proved you are a fool. Thanks for keeping your temper in control");
