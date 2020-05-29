b3f0374af41cc9adf6f318049d87e3e3a5edfd5b
Sachin Kumar Mishra <mishrasachin811@gmail.com>
2020-05-29 23:20:35 +0530

    Add files via upload

---
+++ stuff2.js
@@ -0,0 +1,10 @@
+module.exports.counter = function(arr){
+    return 'there are ' + arr.length + ' elements in the array';
+
+};
+module.exports.adder = function(a,b){
+    return ` the sum of the two numbers are ${a+b}`;
+};
+
+module.exports.pi = 3.142;
+
---
+++ stuff3.js
@@ -0,0 +1,15 @@
+var counter = function(arr){
+    return 'there are ' + arr.length + ' elements in the array';
+
+};
+var adder = function(a,b){
+    return ` the sum of the two numbers are ${a+b}`;
+};
+
+var pi = 3.142;
+
+module.exports = {
+counter : counter,
+adder : adder,
+pi : pi
+};
