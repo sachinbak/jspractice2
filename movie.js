be312fe880f52cbfa229ea5ca2e9fab281a627d3
Sachin Kumar Mishra <mishrasachin811@gmail.com>
2020-06-08 11:32:56 +0530

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,23 @@
+var movies = [
+    {
+        title: "naruto",
+        hasWatched: true,
+        rating: 5
+    },
+    {
+        title: "naruto",
+        hasWatched: false,
+        rating: 5
+    },
+    {
+        title: "naruto",
+        hasWatched: false,
+        rating: 5
+    },
+    {
+        title: "naruto",
+        hasWatched: true,
+        rating: 5
+    }
+]
+
